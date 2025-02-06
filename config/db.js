42add04f9730b6d2f07aa75a2d5824118eb8f14e
ashwani163 <kumarashwani01917@gmail.com>
2025-02-06 16:57:35 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDb = async ()=>{
+    try {
+        const connection = await mongoose.connect("mongodb+srv://shubham1998:0RTclkde5KA94PeN@mymongodb.jy0ll84.mongodb.net/sangam");
+        console.log("Database is connected");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = connectDb;
---
+++ routes/order.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const orderController = require("../controller/order");
+
+
+const router = express.Router();
+
+router.get("/getAllOrders",orderController.getAllOrder);
+router.get("/getOrderByUserId/:id",orderController.getOrderByUserId);
+router.delete("/deleteOrder/:id",orderController.deleteOrder);
+
+
+module.exports = router;
